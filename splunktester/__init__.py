3bc3908f983127fedb4f7c2f6dd45f1d9e50a380
Micah Kemp <micahk@splunk.com>
2020-06-13 18:44:30 -0500

    Trying out some formatting of output, so forcing a failure.

--- splunktester/__init__.py
+++ splunktester/__init__.py
@@ -15,41 +15,38 @@ class SplunkTester(object):
     def test_configs(self, files, app=None, user=None):
         test_service = self._context_service(app=app, user=user, **self._connect_args)
 
+        print(f"User: {user}")
+        print(f"App: {app}")
+
         for test_file_name, test_file_config in files.items():
+            print(f"  Config file: {test_file_name}")
+
             success = True
 
             try:
                 conf_file = test_service.confs[test_file_name]
             except KeyError:
-                print(f"User: {user}")
-                print(f"App: {app}")
-                print(f"Config file: {test_file_name}")
-                print(f"  Expected: present, Got: absent")
+                print(f"!!!!Expected: present, Got: absent")
                 success = False
                 continue
 
             for test_stanza_name, test_stanza_config in test_file_config.items():
+                print(f"    Stanza: {test_stanza_name}")
+
                 try:
                     stanza = conf_file[test_stanza_name]
                 except KeyError:
-                    print(f"User: {user}")
-                    print(f"App: {app}")
-                    print(f"Config file: {test_file_name}")
-                    print(f"  Stanza: {test_stanza_name}")
-                    print(f"    Expected: present, Got: absent")
+                    print(f"!!!!!!Expected: present, Got: absent")
                     success = False
                     continue
 
                 for test_key_name, test_key_value in test_stanza_config.items():
+                    print(f"      Key: {test_key_name}")
+
                     try:
                         key_value = stanza[test_key_name]
                     except KeyError:
-                        print(f"User: {user}")
-                        print(f"App: {app}")
-                        print(f"Config file: {test_file_name}")
-                        print(f"  Stanza: {test_stanza_name}")
-                        print(f"    Key: {test_key_name}")
-                        print(f"      Expected: present, Got: absent")
+                        print(f"!!!!!!!!Expected: present, Got: absent")
                         success = False
                         continue
 
@@ -57,12 +54,7 @@ class SplunkTester(object):
                         # all conf values are returned as strings, so compare appropriately
                         assert key_value == str(test_key_value)
                     except AssertionError:
-                        print(f"User: {user}")
-                        print(f"App: {app}")
-                        print(f"Config file: {test_file_name}")
-                        print(f"  Stanza: {test_stanza_name}")
-                        print(f"    Key: {test_key_name}")
-                        print(f"      Expected: {test_key_value}, Got: {key_value}")
+                        print(f"!!!!!!!!Expected: {test_key_value}, Got: {key_value}")
                         success = False
 
             assert success
